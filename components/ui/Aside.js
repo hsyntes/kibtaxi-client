df62735b92bba133a4aaf024b6f8d48486866625
hsyntes <se.hsyntes@gmail.com>
2024-09-30 16:47:03 +0300

    listing other taxis around users

--- components/ui/Aside.js
+++ components/ui/Aside.js
@@ -1,4 +1,4 @@
-import { faLocation } from "@fortawesome/free-solid-svg-icons";
+import { faRocket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CardTaxi from "./Cards/CardTaxi";
 
@@ -7,15 +7,19 @@ const Aside = ({ popular_taxis }) => {
 
   return (
     <aside id="app-aside" className="border-r dark:border-r-dark p-4">
-      <h6 className="flex items-center gap-2 font-semibold mb-4">
-        <FontAwesomeIcon icon={faLocation} className="text-primary" />
-        <span>Most popular, Famagusta</span>
-      </h6>
-      <ul className="space-y-6">
-        {popular_taxis.map((popular_taxi) => (
-          <CardTaxi taxi={popular_taxi} key={popular_taxi.taxi_placeId} />
-        ))}
-      </ul>
+      <section className="mb-4">
+        <h6 className="flex items-center gap-2 font-semibold">
+          <FontAwesomeIcon icon={faRocket} className="text-primary" />
+          <span>Most popular, Famagusta</span>
+        </h6>
+      </section>
+      <section>
+        <ul className="space-y-6">
+          {popular_taxis.map((popular_taxi) => (
+            <CardTaxi taxi={popular_taxi} key={popular_taxi.taxi_placeId} />
+          ))}
+        </ul>
+      </section>
     </aside>
   );
 };
